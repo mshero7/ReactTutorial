6e351477cf374bd621044c9b8c5cce48c9a1d720
mshero7 <mshero7@naver.com>
2020-03-25 01:08:32 +0900

    customer.js 선언 및 계층화 예제

--- src/App.js
+++ src/App.js
@@ -1,14 +1,65 @@
 import React, {Component} from 'react';
 import './App.css';
-import logo from './logo.svg';
+import Customer from './component/Customer'
+
+const customer = {
+  'id' : 1,
+  'image' : 'https://placeimg.com/64/64/any',
+  'name' : '문상수',
+  'birthday' : '961222',
+  'gender' : '남자',
+  'job' : '대학생'
+}
+
+const customers = [
+  {
+    'id' : 1,
+    'image' : 'https://placeimg.com/64/64/1',
+    'name' : '문상수1',
+    'birthday' : '961222',
+    'gender' : '남자',
+    'job' : '대학생'
+  },
+  {
+    'id' : 2,
+    'image' : 'https://placeimg.com/64/64/2',
+    'name' : '문상수2',
+    'birthday' : '961222',
+    'gender' : '남자',
+    'job' : '대학생'
+  },
+  {
+    'id' : 3,
+    'image' : 'https://placeimg.com/64/64/3',
+    'name' : '문상수3',
+    'birthday' : '961222',
+    'gender' : '남자',
+    'job' : '대학생'
+  }
+]
 
 class App extends Component {
   render() {
     return (
-      <div className="gray-background">
-      <img src={logo} className="App-logo" alt="logo" />
-        <h2>Let</h2>
+      <div>
+        {
+          customers.map(c => {
+            return (
+              <Customer
+              key={c.id}
+              id={c.id}
+              image={c.image}
+              name={c.name}
+              birthday={c.birthday}
+              gender={c.gender}
+              job={c.job}
+              />
+            )
+          })
+        }
       </div>
+      
+      
     )
   }
 }
